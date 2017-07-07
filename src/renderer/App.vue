afc2dc4d1abf598cf70738648b6a24857cd96fe9
Flascher <alexanderflasch@gmail.com>
2017-07-07 15:58:52 -0500

    First commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'activity-card-tracker'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
